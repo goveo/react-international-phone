783fb766297ec31bf111442ed8681ab3b45264aa
Yurii Brusentsov <yuriybrusentsov@gmail.com>
2022-10-25 00:12:06 +0300

    chore: extend PhoneInput stories

--- src/stories/PhoneInput.stories.tsx
+++ src/stories/PhoneInput.stories.tsx
@@ -14,23 +14,24 @@ const Template: ComponentStory<typeof PhoneInput> = (args) => (
   <PhoneInput {...args} />
 );
 
-export const Base = Template.bind({});
-
-Base.argTypes = {
-  initialCountry: {
-    options: countries.map((c) => parseCountry(c).iso2),
-    control: {
-      type: 'select',
-      labels: countries.reduce((acc: Record<string, string>, c) => {
-        const { name, iso2 } = parseCountry(c);
-        acc[iso2] = `${name} (${iso2})`;
-        return acc;
-      }, {}),
-    },
+const initialCountryArgType = {
+  options: countries.map((c) => parseCountry(c).iso2),
+  control: {
+    type: 'select',
+    labels: countries.reduce((acc: Record<string, string>, c) => {
+      const { name, iso2 } = parseCountry(c);
+      acc[iso2] = `${name} (${iso2})`;
+      return acc;
+    }, {}),
   },
 };
 
-Base.args = {
+export const Default = Template.bind({});
+Default.argTypes = {
+  initialCountry: initialCountryArgType,
+};
+
+Default.args = {
   disabled: false,
   hideDropdown: false,
   initialCountry: 'us',
@@ -42,4 +43,57 @@ Base.args = {
   forceDialCode: false,
   disableDialCodeAndPrefix: false,
   showDisabledDialCodeAndPrefix: false,
+};
+
+export const ForcedDialCode = Template.bind({});
+ForcedDialCode.argTypes = {
+  initialCountry: initialCountryArgType,
+};
+
+ForcedDialCode.args = {
+  initialCountry: 'ua',
+  forceDialCode: true,
+};
+
+export const DisabledPrefill = Template.bind({});
+DisabledPrefill.argTypes = {
+  initialCountry: initialCountryArgType,
+};
+
+DisabledPrefill.args = {
+  initialCountry: 'pl',
+  disableDialCodePrefill: true,
+};
+
+export const DisabledCountryGuess = Template.bind({});
+DisabledCountryGuess.argTypes = {
+  initialCountry: initialCountryArgType,
+};
+
+DisabledCountryGuess.args = {
+  initialCountry: 'gb',
+  disableCountryGuess: true,
+};
+
+export const WithoutDropdown = Template.bind({});
+WithoutDropdown.argTypes = {
+  initialCountry: initialCountryArgType,
+};
+
+WithoutDropdown.args = {
+  initialCountry: 'ee',
+  disableCountryGuess: true,
+  hideDropdown: true,
+  forceDialCode: true,
+};
+
+export const WithCodePreview = Template.bind({});
+WithCodePreview.argTypes = {
+  initialCountry: initialCountryArgType,
+};
+
+WithCodePreview.args = {
+  initialCountry: 'fi',
+  disableDialCodeAndPrefix: true,
+  showDisabledDialCodeAndPrefix: true,
 };
