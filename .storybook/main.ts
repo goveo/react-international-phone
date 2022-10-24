c0c220897a5e8c64ab45daf85fd6b264b1c02a53
Yurii Brusentsov <yuriybrusentsov@gmail.com>
2022-10-25 00:29:42 +0300

    fix(storybook): default export error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,8 @@
-export default {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-controls', '@storybook/preset-scss'],
-  core: {
-    builder: '@storybook/builder-webpack5',
-  },
+export const stories = [
+  '../src/**/*.stories.mdx',
+  '../src/**/*.stories.@(js|jsx|ts|tsx)',
+];
+export const addons = ['@storybook/addon-controls', '@storybook/preset-scss'];
+export const core = {
+  builder: '@storybook/builder-webpack5',
 };
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,3 @@
-export default {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-  },
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
 };
