4b98d5d0804eeeebd6fda85c92685dd0826c0346
Yurii Brusentsov <yuriybrusentsov@gmail.com>
2022-12-11 00:04:54 +0200

    imp(build): update export with types and FlagEmoji

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 export { CountrySelector } from './components/CountrySelector/CountrySelector';
 export { CountrySelectorDropdown } from './components/CountrySelector/CountrySelectorDropdown';
 export { DialCodePreview } from './components/DialCodePreview/DialCodePreview';
+export { FlagEmoji } from './components/FlagEmoji/FlagEmoji';
+export type { PhoneInputProps } from './components/PhoneInput/PhoneInput';
 export { PhoneInput } from './components/PhoneInput/PhoneInput';
 export { defaultCountries } from './data/countryData';
+export type { UsePhoneConfig } from './hooks/usePhone';
 export { usePhone } from './hooks/usePhone';
 export { usePhoneInput } from './hooks/usePhoneInput';
 export type { CountryIso2 } from './types';
