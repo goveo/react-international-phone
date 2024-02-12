59181dee932e2b12ec2fa8d9067b689c755a18c7
Yurii Brusentsov <yuriybrusentsov@gmail.com>
2024-02-12 23:39:11 +0200

    fix(types): export missing types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+export type { CountrySelectorProps } from './components/CountrySelector/CountrySelector';
 export { CountrySelector } from './components/CountrySelector/CountrySelector';
+export type { CountrySelectorDropdownProps } from './components/CountrySelector/CountrySelectorDropdown';
 export { CountrySelectorDropdown } from './components/CountrySelector/CountrySelectorDropdown';
+export type { DialCodePreviewProps } from './components/DialCodePreview/DialCodePreview';
 export { DialCodePreview } from './components/DialCodePreview/DialCodePreview';
+export type { FlagImageProps } from './components/FlagImage/FlagImage';
 export { FlagImage } from './components/FlagImage/FlagImage';
 export type {
   PhoneInputProps,
@@ -8,8 +12,9 @@ export type {
 } from './components/PhoneInput/PhoneInput';
 export { PhoneInput } from './components/PhoneInput/PhoneInput';
 export { defaultCountries } from './data/countryData';
+export type { UsePhoneInputConfig } from './hooks/usePhoneInput';
 export { usePhoneInput } from './hooks/usePhoneInput';
-export type { CountryData, CountryIso2 } from './types';
+export * from './types';
 export {
   buildCountryData,
   getActiveFormattingMask,
